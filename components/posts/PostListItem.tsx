5fe85624998c141f0c7241e9acfe5d47470be3a1
Asami Takaoka <asamiyamabe61@gmail.com>
2023-12-20 20:22:00 +0900

    onMouseOverとonMouseOutを使用しマウスが要素の上にあるときにisHoveredをtrueにする

--- components/posts/PostListItem.tsx
+++ components/posts/PostListItem.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { PostData } from "types/types";
+import { useState } from "react";
 
 type Props = {
   post: PostData;
@@ -7,20 +8,26 @@ type Props = {
 
 const PostListItem = ({ post }: Props) => {
   const date = new Date(post.created_at);
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="py-8 flex flex-wrap md:flex-nowrap">
+    <div
+      className={`py-8 flex flex-wrap md:flex-nowrap ${
+        isHovered ? "cursor-pointer" : ""
+      }`}
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+    >
       <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
         <span className="mt-1 text-gray-500 text-sm">
           {date.toDateString()}
         </span>
       </div>
       <div className="md:flex-grow">
-        <Link
-          href={`/posts/${encodeURIComponent(post.id)}`}
-          className="text-2xl font-medium text-gray-800 title-font mb-2"
-        >
-          {post.title}
+        <Link href={`/posts/${encodeURIComponent(post.id)}`} legacyBehavior>
+          <a className={`text-2xl font-medium text-gray-800 title-font mb-2 ${isHovered ? "text-blue-500 underline" : ""}`}>
+            {post.title}
+          </a>
         </Link>
         <p className="leading-relaxed">{post.body}</p>
       </div>
