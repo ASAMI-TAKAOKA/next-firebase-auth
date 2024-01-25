b1175a85296fa0324ea8d16c99ffb7fabe54ece9
Asami Takaoka <asamiyamabe61@gmail.com>
2024-01-25 17:05:42 +0900

    ページネーション部分をコンポーネント化

---
+++ components/Pagination.tsx
@@ -0,0 +1,58 @@
+import PostList from "components/posts/PostList";
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { PostData } from "types/types";
+
+type Props = {
+  posts: PostData[];
+};
+
+const Pagination = (props: Props) => {
+
+  const { posts } = props;
+  const itemsPerPage = 6;
+
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const endOffset = itemOffset + itemsPerPage;
+  // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentPosts = posts.slice(itemOffset, endOffset);
+
+  const pageCount = Math.ceil(posts.length / itemsPerPage);
+
+  const handlePageClick = (e: { selected: number }) => {
+    const newOffset = (e.selected * itemsPerPage) % posts.length;
+    console.log(
+      `User requested page number ${e.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+  
+
+  return (
+    <div className="px-40">
+        <PostList posts={posts} currentPosts={currentPosts}/>
+        <ReactPaginate
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={2}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          pageClassName="page-item"
+          pageLinkClassName="page-link"
+          previousClassName="page-item"
+          previousLinkClassName="page-link"
+          nextClassName="page-item"
+          nextLinkClassName="page-link"
+          breakLabel="..."
+          breakClassName="page-item"
+          breakLinkClassName="page-link"
+          containerClassName="pagination"
+          activeClassName="active"
+        />
+    </div>
+  );
+}
+export default Pagination;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,14 @@ import Head from "next/head";
 import { GetServerSideProps } from "next";
 import fetch from "node-fetch";
 import { PostData } from "types/types";
-import PostList from "components/posts/PostList";
 import 'react-tabs/style/react-tabs.css';
-import { useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Pagination from "components/Pagination";
 
 type Props = {
   posts: PostData[];
 };
 
 export default function HomePage({ posts }: Props) {
-
-  const itemsPerPage = 6;
-
-  const [itemOffset, setItemOffset] = useState(0);
-
-  const endOffset = itemOffset + itemsPerPage;
-  // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-  const currentPosts = posts.slice(itemOffset, endOffset);
-
-  const pageCount = Math.ceil(posts.length / itemsPerPage);
-
-  const handlePageClick = (e: { selected: number }) => {
-    const newOffset = (e.selected * itemsPerPage) % posts.length;
-    console.log(
-      `User requested page number ${e.selected}, which is offset ${newOffset}`
-    );
-    setItemOffset(newOffset);
-  };
   return (
     <>
       <Head>
@@ -41,33 +20,10 @@ export default function HomePage({ posts }: Props) {
           content="This is a app for mom and baby using firebase authenticate"
         />
       </Head>
-      <section className="mx-auto py-10 px-8">
-        <div className="px-40">
-          <PostList posts={posts} currentPosts={currentPosts}/>
-          <ReactPaginate
-            nextLabel="next >"
-            onPageChange={handlePageClick}
-            pageRangeDisplayed={3}
-            marginPagesDisplayed={2}
-            pageCount={pageCount}
-            previousLabel="< previous"
-            pageClassName="page-item"
-            pageLinkClassName="page-link"
-            previousClassName="page-item"
-            previousLinkClassName="page-link"
-            nextClassName="page-item"
-            nextLinkClassName="page-link"
-            breakLabel="..."
-            breakClassName="page-item"
-            breakLinkClassName="page-link"
-            containerClassName="pagination"
-            activeClassName="active"
-          />
-        </div>
-      </section>
+      <Pagination posts={posts} />
     </>
   );
-}
+};
 
 export const getServerSideProps: GetServerSideProps<Props> = async (
   context
