1be54a7f34f7915fd7dd72dd64db8f6e881d5f23
Asami Takaoka <asamiyamabe61@gmail.com>
2024-01-28 16:49:00 +0900

    react-paginate修正

--- components/layout/Pagination.tsx
+++ components/layout/Pagination.tsx
@@ -37,37 +37,38 @@ const Pagination = (props: Props) => {
   
 
   return (
-    <div>
-      <PostList
-        currentPosts={currentPosts}
-      />
-    {/* TODO: styles.paginationの@import "bootstrap";の部分が適用されない */}
-    {/* <div className={`${styles['pagination']} px-40`}> */}
-    <div className={styles.pagination}>
-    {/* <div> */}
-      <ReactPaginate
-        pageCount={pageCount}
-        onPageChange={handlePageClick}
-        marginPagesDisplayed={2} //先頭と末尾に表示するページの数。今回は2としたので1,2…今いるページの前後…後ろから2番目, 1番目 のように表示されます。
-        pageRangeDisplayed={5} //上記の「今いるページの前後」の番号をいくつ表示させるかを決めます。
-        containerClassName='pagination' //ページネーションリンクの親要素のクラス名
-        pageClassName='page-item' //各子要素(li要素)のクラス名
-        pageLinkClassName='page-link' //ページネーションのリンクのクラス名
-        activeClassName='active' //今いるページ番号のクラス名。今いるページの番号だけ太字にしたりできます 
-        previousLabel='<' //前のページ番号に戻すリンクのテキスト
-        nextLabel='>' //次のページに進むボタンのテキスト
-        previousClassName='page-item' // '<'の親要素(li)のクラス名
-        nextClassName='page-item' //'>'の親要素(li)のクラス名
-        previousLinkClassName='page-link'  //'<'のリンクのクラス名
-        nextLinkClassName='page-link' //'>'のリンクのクラス名
-        disabledClassName='disabled' //先頭 or 末尾に行ったときにそれ以上戻れ(進め)なくするためのクラス
-        breakLabel='...' // ページがたくさんあるときに表示しない番号に当たる部分をどう表示するか
-        breakClassName='page-item' // 上記の「…」のクラス名
-        breakLinkClassName='page-link' // 「…」の中のリンクにつけるクラス
-        renderOnZeroPageCount={null}
-      />
-    </div>
-    </div>
+    <section>
+      <div>
+        <PostList
+          currentPosts={currentPosts}
+        />
+      {/* TODO: styles.paginationの@import "bootstrap";の部分が適用されない */}
+      {/* <div className={`${styles['pagination']} px-40`}> */}
+      <div className={styles.pagination}>
+        <ReactPaginate
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          marginPagesDisplayed={2} //先頭と末尾に表示するページの数。今回は2としたので1,2…今いるページの前後…後ろから2番目, 1番目 のように表示されます。
+          pageRangeDisplayed={5} //上記の「今いるページの前後」の番号をいくつ表示させるかを決めます。
+          containerClassName='pagination' //ページネーションリンクの親要素のクラス名
+          pageClassName='page-item' //各子要素(li要素)のクラス名
+          pageLinkClassName='page-link' //ページネーションのリンクのクラス名
+          activeClassName='active' //今いるページ番号のクラス名。今いるページの番号だけ太字にしたりできます 
+          previousLabel='<' //前のページ番号に戻すリンクのテキスト
+          nextLabel='>' //次のページに進むボタンのテキスト
+          previousClassName='page-item' // '<'の親要素(li)のクラス名
+          nextClassName='page-item' //'>'の親要素(li)のクラス名
+          previousLinkClassName='page-link'  //'<'のリンクのクラス名
+          nextLinkClassName='page-link' //'>'のリンクのクラス名
+          disabledClassName='disabled' //先頭 or 末尾に行ったときにそれ以上戻れ(進め)なくするためのクラス
+          breakLabel='...' // ページがたくさんあるときに表示しない番号に当たる部分をどう表示するか
+          breakClassName='page-item' // 上記の「…」のクラス名
+          breakLinkClassName='page-link' // 「…」の中のリンクにつけるクラス
+          renderOnZeroPageCount={null}
+        />
+      </div>
+      </div>
+    </section>
   );
 }
 export default Pagination;
