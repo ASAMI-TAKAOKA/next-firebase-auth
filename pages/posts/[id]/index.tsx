3cc756d112b9991d4c828b3f1a9cf919c2fbf666
Asami Takaoka <asamiyamabe61@gmail.com>
2024-02-15 15:33:07 +0900

    不要なコメント削除

--- pages/posts/[id]/index.tsx
+++ pages/posts/[id]/index.tsx
@@ -68,7 +68,6 @@ export default function PostDetailPage({ post }: Props) {
               src={postImage}
             />
           </div>
-          {/* 表示されない範囲 */}
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               {post.title}
