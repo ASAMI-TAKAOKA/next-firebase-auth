d65c95fefd3cd37de57404037c65606662952b98
Asami Takaoka <asamiyamabe61@gmail.com>
2024-02-16 14:43:44 +0900

    コメント用のアイコンをtitleとbodyと同じ階層に移動

--- components/posts/PostListItem.tsx
+++ components/posts/PostListItem.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { PostData } from "types/types";
 import { useState } from "react";
-import { HiOutlineChatAlt } from "react-icons/hi"; // Tailwind CSSのアイコンをインポート
+import { HiOutlineChatAlt } from "react-icons/hi";
 
 type Props = {
   post: PostData;
@@ -19,29 +19,30 @@ const PostListItem = ({ post }: Props) => {
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
     >
-      <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+      <div className="md:w-64 md:mb-0 mb-6">
         <span className="mt-1 text-gray-500 text-sm">
           {date.toDateString()}
         </span>
       </div>
-      <div className="md:flex-grow flex flex-col md:flex-row w-full md:w-auto">
+      {/* TODO: レスポンシブにするためにあとでclassを追加 */}
+      <div className="">
         <Link href={`/posts/${encodeURIComponent(post.id)}`} legacyBehavior>
           <a className={`text-2xl font-medium text-gray-600 title-font mb-2 md:mb-0 ${isHovered ? "underline" : ""}`}>
             {post.title}
           </a>
         </Link>
         <p className="leading-relaxed text-gray-600">{post.body}</p>
+        {/* コメント用のアイコン */}
+        <Link href={`/posts/${encodeURIComponent(post.id)}`} legacyBehavior>
+          <a>
+            <button
+              className=" items-center justify-center w-10 h-10 text-gray-600 focus:outline-none hover:bg-gray-300"
+            >
+              <HiOutlineChatAlt size={24}/>
+            </button>
+          </a>
+        </Link>
       </div>
-      {/* コメント用のアイコン */}
-      <Link href={`/posts/${encodeURIComponent(post.id)}`} legacyBehavior>
-        <a>
-          <button
-            className="inline-flex items-center justify-center w-10 h-10 text-gray-600 focus:outline-none hover:bg-gray-300"
-          >
-            <HiOutlineChatAlt size={24}/>
-          </button>
-        </a>
-      </Link>
     </div>
   );
 };
