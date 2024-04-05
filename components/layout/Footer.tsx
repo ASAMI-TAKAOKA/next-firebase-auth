5eb4783ffcf1a7f2c9f2148e580b2f97ecd0149c
Asami Takaoka <asamiyamabe61@gmail.com>
2024-04-05 13:41:03 +0900

    smでもfooterが中央にくるように修正

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className=" mx-auto px-6 py-6">
       <hr className="my-6 border-gray-200 lg:my-8" />
-      <span className="block text-sm text-gray-500 sm:text-center">
+      <span className="block text-sm text-gray-500 text-center">
         <a
           href="https://github.com/ASAMI-TAKAOKA/next-firebase-auth"
           className="hover:underline"
