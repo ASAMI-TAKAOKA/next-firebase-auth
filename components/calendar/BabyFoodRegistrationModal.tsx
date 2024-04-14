c1102bb4a4008847ea5a6e0fd2e4f91c3fb97225
Asami Takaoka <asamiyamabe61@gmail.com>
2024-04-14 13:35:52 +0900

    モーダルのデザインをアプリ全体に合わせる

--- components/calendar/BabyFoodRegistrationModal.tsx
+++ components/calendar/BabyFoodRegistrationModal.tsx
@@ -5,25 +5,23 @@ export type ModalProps = {
   clickOkToCloseTheModal: () => void;
 };
 
-const BabyFoodRegistrationModal = (props: ModalProps) => {
-  return props.open ? (
-    <>
-      <div className="bg-white  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-48 p-5 flex flex-col items-start absolute z-20">
-        <h1 className="text-xl font-bold mb-5">Title</h1>
-        <p className="text-lg mb-5">Modal Message.</p>
-        <div className="flex mt-auto w-full">
+const BabyFoodRegistrationModal = ({ open, clickOkToCloseTheModal }: ModalProps) => {
+  return open ? (
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
+      <div className="rounded-md bg-white w-80 h-auto p-5 flex flex-col items-center">
+        <h1 className="text-gray-700 text-xl font-bold mb-5">Title</h1>
+        <p className="text-gray-700 text-lg mb-5">Modal Message.</p>
+        <div className="flex justify-center w-full">
           <button
-            className="bg-slate-900 hover:bg-slate-700 text-white px-8 py-2 mx-auto"
-            onClick={() => props.clickOkToCloseTheModal()}
+            className="bg-pink-400 hover:bg-pink-700 text-white px-8 py-2 rounded-md focus:outline-none focus:ring font-medium"
+            onClick={clickOkToCloseTheModal}
           >
             OK
           </button>
         </div>
       </div>
-    </>
-  ) : (
-    <></>
-  );
+    </div>
+  ) : null;
 };
 
 export default BabyFoodRegistrationModal;
